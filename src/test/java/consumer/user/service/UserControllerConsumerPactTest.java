ea033752c60ad11a2dff29b20ecca9ccb416e116
Hansjoerg Keser <hansjoerg.keser@bit.admin.ch>
2021-01-24 18:07:52 +0100

    pact consumer test with 2 interactions

--- src/test/java/consumer/user/service/UserControllerConsumerPactTest.java
+++ src/test/java/consumer/user/service/UserControllerConsumerPactTest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = {UserController.class, UserClient.class})
 @AutoConfigureWebClient
-@PactTestFor(port = "8080")
+@PactTestFor(port = "8083")
 //@TestPropertySource(properties = {
 // enable if test fails to get more details in log
 //        "logging.level.au.com.dius.pact=DEBUG"
@@ -39,7 +39,7 @@ public class UserControllerConsumerPactTest {
 
     private UserDTO createUserDTO() {
         SkillDTO skillDTO1 = new SkillDTO();
-        skillDTO1.setSkillId("9");
+        skillDTO1.setSkillId("456");
         skillDTO1.setSkillName("Creating Pact Consumer Tests");
 
         UserDTO exampleUserDTO = new UserDTO();
@@ -61,10 +61,10 @@ public class UserControllerConsumerPactTest {
                 .body(
                         // https://docs.pact.io/implementation_guides/jvm/consumer
                         PactDslJsonArray.arrayEachLike()
-                                .stringType("userId", "123")
+                                .integerType("userId", 123)
                                 .stringType("userName", "Pactman")
                                 .eachLike("skillDtos")
-                                .stringType("skillId", "9")
+                                .integerType("skillId", 456)
                                 .stringType("skillName", "Creating Pact Consumer Tests")
                                 .closeArray()
                                 .closeObject()
@@ -78,6 +78,41 @@ public class UserControllerConsumerPactTest {
         List<UserDTO> responseOfMockServer = assertDoesNotThrow(() -> userController.getAllUsers());
         assertFalse(responseOfMockServer.isEmpty(), "Response must not be empty, because we defined the body, having an array with one example dto.");
         assertEquals(Collections.singletonList(createUserDTO()), responseOfMockServer, "Response body contained a different user dto as expected.");
+    }
+
+    private static final String USER_ID_PARAM_NAME = "userId";
+    public static final int USER_ID_EXAMPLE_VALUE = 101;
+
+    @Pact(provider = "PactDemoProvider", consumer = "PactDemoConsumer")
+    public RequestResponsePact createPact_GetAllUsersWithProviderState(PactDslWithProvider builder) {
+
+        return builder
+                .given("User with user id ${" + USER_ID_PARAM_NAME + "} exists", USER_ID_PARAM_NAME, USER_ID_EXAMPLE_VALUE)
+                .uponReceiving("getAllUsers test interaction response for user with " + USER_ID_PARAM_NAME + " = " + USER_ID_EXAMPLE_VALUE)
+                .path("/user/getAllUsers")
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .body(
+                        // https://docs.pact.io/implementation_guides/jvm/consumer
+                        PactDslJsonArray.arrayEachLike()
+                                .integerType(USER_ID_PARAM_NAME, USER_ID_EXAMPLE_VALUE)
+                                .stringType("userName", "Pactman")
+                                .eachLike("skillDtos")
+                                .integerType("skillId", 202)
+                                .stringType("skillName", "Creating Pact Consumer Tests")
+                                .closeArray()
+                                .closeObject()
+                )
+                .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "createPact_GetAllUsersWithProviderState")
+    public void testCreatePact_GetAllUsersWithProviderState() {
+        List<UserDTO> responseOfMockServer = assertDoesNotThrow(() -> userController.getAllUsers());
+        assertFalse(responseOfMockServer.isEmpty(), "Response must not be empty, because we defined the body, having an array with one example dto.");
+        assertEquals(String.valueOf(USER_ID_EXAMPLE_VALUE), responseOfMockServer.get(0).getUserId());
     }
 
 }
