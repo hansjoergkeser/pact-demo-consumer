b90e88af0043e06018f1aab3ca7faecaaeca0b36
Keser Hansjörg (IT-PTR-EXT - Extern) <hansjoerg.keser@sbb.ch>
2022-08-24 00:11:24 +0200

    removed test

--- src/main/java/org/example/consumer/user/service/controller/UserClient.java
+++ src/main/java/org/example/consumer/user/service/controller/UserClient.java
@@ -1,5 +1,6 @@
 package org.example.consumer.user.service.controller;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.example.consumer.user.service.schema.UserDTO;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +12,6 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
-import java.util.List;
-
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 
@@ -20,51 +19,55 @@ import static java.util.Collections.emptyList;
 @Service
 public class UserClient {
 
-    private final WebClient webClient;
+	private final WebClient webClient;
 
-    @Value("${user.service.url}")
-    private String userServiceUrl;
+	@Value("${user.service.url}")
+	private String userServiceUrl;
 
-    public UserClient(WebClient.Builder webClientBuilder,
-                      @Value("${user.service.url}") String userServiceUrl) {
-        this.webClient = webClientBuilder
-                .baseUrl(userServiceUrl)
-                // only needed for IntelliJ
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()))
-                .filter(logRequest())
-                .build();
-    }
+	public UserClient(WebClient.Builder webClientBuilder,
+					  @Value("${user.service.url}") String userServiceUrl) {
+		this.webClient = webClientBuilder
+				.baseUrl(userServiceUrl)
+				// only needed for IntelliJ
+				.clientConnector(new ReactorClientHttpConnector(HttpClient.create()))
+				.filter(logRequest())
+				.build();
+	}
 
-    // This method returns filter function which will log request data
-    private static ExchangeFilterFunction logRequest() {
-        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
-            log.info("Outgoing request: {} {}", clientRequest.method(), clientRequest.url());
-            clientRequest.headers().forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
-            return Mono.just(clientRequest);
-        });
-    }
+	// This method returns filter function which will log request data
+	private static ExchangeFilterFunction logRequest() {
+		return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+			log.info("Outgoing request: {} {}", clientRequest.method(), clientRequest.url());
+			clientRequest.headers().forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
+			return Mono.just(clientRequest);
+		});
+	}
 
-    public UserDTO getUserById(String id) {
-        return webClient.get().
-                uri("/getUser/{id}", id).
-                retrieve().
-                bodyToMono(UserDTO.class).
-                onErrorMap(WebClientResponseException.BadRequest.class,
-                        e -> new Exception("BadRequest on user API.", e)).
-                block();
-    }
+	public UserDTO getUserById(String id) {
+		return webClient.get().
+				uri("/getUser/{id}", id).
+				retrieve().
+				bodyToMono(UserDTO.class).
+				onErrorMap(WebClientResponseException.BadRequest.class,
+						e -> new Exception("BadRequest on user API.", e)).
+				block();
+	}
 
-    public List<UserDTO> getAllUsers() {
-        try {
-            UserDTO[] userArray = webClient.get().uri("/getAllUsers").retrieve().bodyToMono(UserDTO[].class).block();
+	public List<UserDTO> getAllUsers() {
+		try {
+			UserDTO[] userArray = webClient.get().
+					uri("/getAllUsers").
+					retrieve().
+					bodyToMono(UserDTO[].class).
+					block();
 
-            if (userArray != null) {
-                return asList(userArray);
-            }
-        } catch (Exception e) {
-            log.error("getAllUsers failed: " + e);
-        }
-        return emptyList();
-    }
+			if (userArray != null) {
+				return asList(userArray);
+			}
+		} catch (Exception e) {
+			log.error("getAllUsers failed: " + e);
+		}
+		return emptyList();
+	}
 
 }
